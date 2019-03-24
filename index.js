a35b13f108603114b7c70671d534135cba58677f
Veronica Miller <vmcmiller@gmail.com>
2019-03-24 18:35:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let mealId = 0;
+let neighborhoodId = 0;
+let deliveryId = 0;
+
+class Meal {
+  
+};
+
+class Neighborhood {
+  constructor(name) {
+    
+  }
+};
+
+class Delivery {
+  
+};

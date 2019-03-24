e35a386853d3e436b72be5a45ef191123a0c8646
Veronica Miller <vmcmiller@gmail.com>
2019-03-24 19:34:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ class Neighborhood {
   };
 
   customers() {
-    return this.deliveries().map(function(delivery) { return delivery.customer();
+    return this.deliveries().set(function(delivery) { return delivery.customer();
     }.bind(this));
   };
 
